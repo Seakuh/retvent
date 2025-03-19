91c9a5da0a6d8d9bd3ae7f14834362c880521d38
Seakuh <eteranimal@protonmail.com>
2025-03-19 13:15:26 +0100

    add blank event

--- frontend/src/components/EventPage/EventSection.tsx
+++ frontend/src/components/EventPage/EventSection.tsx
@@ -6,16 +6,31 @@ interface EventSectionProps {
   events: Event[];
 }
 
+const blankEvent: Event = {
+  id: "",
+  title: "No events found",
+  startDate: new Date(),
+  description: "N/A",
+  imageUrl:
+    "https://hel1.your-objectstorage.com/imagebucket/events/8d703697-caf7-4438-abda-4ccd8e5939e9.png",
+};
+
 export const EventSection = ({ title, events }: EventSectionProps) => {
   return (
     <>
       <h1 className="section-title">{title}</h1>
       <div className="event-list-container">
-        {events.map((event) => (
-          <div className="event-card-list-item" key={event.id + title}>
-            <EventCard event={event} />
+        {events.length === 0 ? (
+          <div className="no-events-container">
+            <EventCard event={blankEvent} />
           </div>
-        ))}
+        ) : (
+          events.map((event) => (
+            <div className="event-card-list-item" key={event.id + title}>
+              <EventCard event={event} />
+            </div>
+          ))
+        )}
       </div>
     </>
   );
