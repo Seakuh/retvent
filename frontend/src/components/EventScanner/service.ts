64f2618094ed6117b69d1f105f3f2f2ab0a3a813
Seakuh <eteranimal@protonmail.com>
2025-03-27 20:07:33 +0100

    add v4

--- frontend/src/components/EventScanner/service.ts
+++ frontend/src/components/EventScanner/service.ts
@@ -19,7 +19,7 @@ export const uploadEventImage = async (
 
     // Token holen & API-Endpunkt setzen
     const endpoint = localStorage.getItem("access_token")
-      ? `${API_URL}events/v2/upload/event-image`
+      ? `${API_URL}events/v4/upload/event-image`
       : `${API_URL}events/upload/event-image`;
 
     const token = localStorage.getItem("access_token");
