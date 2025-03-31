13da74fcc3b3b67c967a5a4a2ab41d4b2cc707dd
Seakuh <eteranimal@protonmail.com>
2025-03-31 11:56:38 +0200

    adjust meta tags

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -154,9 +154,9 @@ function LandingPage() {
       <Helmet>
         <title>EventScanner</title>
         <meta name="description" content="EventScanner" />
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 
         <title>
           EventScanner - Discover Events Near You | Real-time Updates
