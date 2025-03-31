fe21dcc11aabd2a01797e3161878e8907445624a
Seakuh <eteranimal@protonmail.com>
2025-03-31 21:16:32 +0200

    updadte links

--- frontend/src/components/User/Profile/ProfileLinks.tsx
+++ frontend/src/components/User/Profile/ProfileLinks.tsx
@@ -5,24 +5,31 @@ interface ProfileLinksProps {
   links: string[];
 }
 
-const getLinkName = (link: string) => {
-  // get domain
-  const domain = new URL(link).hostname;
-  return domain.replace("www.", "");
-};
-
 export const ProfileLinks = ({ links }: ProfileLinksProps) => {
+  // show only 5 links
+  const linksArray: string[] = links.slice(0, 5);
+
+  const getLinkName = (link: string): string => {
+    try {
+      const url = new URL(link);
+      return url.hostname.replace("www.", "");
+    } catch (error) {
+      console.log("Fehler beim Parsen der URL:", error);
+      return link;
+    }
+  };
+
   return (
     <div className="profile-links-container">
       <Link size={17} />
       <div className="profile-links">
-        {links.map((link) => (
+        {linksArray.map((link, index) => (
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="profile-link"
-            key={link}
+            className="profile-link-item"
+            key={link + index}
           >
             {getLinkName(link)}
           </a>
