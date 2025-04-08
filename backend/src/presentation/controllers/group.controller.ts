bbb948d1a6fffcc6604685f428d8b769fc532c8f
Seakuh <eteranimal@protonmail.com>
2025-04-08 15:32:46 +0200

    change from userId to id

--- backend/src/presentation/controllers/group.controller.ts
+++ backend/src/presentation/controllers/group.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { GroupService } from 'src/application/services/group.service';
 import { CreateGroupDto } from '../dtos/create-group.dto';
+import { UpdateGroupDto } from '../dtos/update-group.dto';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('groups')
@@ -10,6 +20,34 @@ export class GroupController {
   @Post()
   @UseGuards(JwtAuthGuard)
   create(@Req() req, @Body() dto: CreateGroupDto) {
-    return this.groupService.createGroup(req.user.userId, dto);
+    return this.groupService.createGroup(req.user.id, dto);
+  }
+
+  @Post('/join/:token')
+  @UseGuards(JwtAuthGuard)
+  joinGroup(@Req() req, @Param('token') token: string) {
+    return this.groupService.joinGroup(req.user.id, token);
+  }
+
+  @Post('/leave/:groupId')
+  @UseGuards(JwtAuthGuard)
+  leaveGroup(@Req() req, @Param('groupId') groupId: string) {
+    return this.groupService.leaveGroup(req.user.id, groupId);
+  }
+
+  @Get('/:groupId')
+  @UseGuards(JwtAuthGuard)
+  getGroup(@Req() req, @Param('groupId') groupId: string) {
+    return this.groupService.getGroupById(groupId);
+  }
+
+  @Put('/:groupId')
+  @UseGuards(JwtAuthGuard)
+  updateGroup(
+    @Req() req,
+    @Param('groupId') groupId: string,
+    @Body() dto: UpdateGroupDto,
+  ) {
+    return this.groupService.updateGroup(groupId, dto);
   }
 }
