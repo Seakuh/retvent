00816e7a6190e2a45ecdd83046c3d5a8f038966a
Seakuh <eteranimal@protonmail.com>
2025-04-08 15:19:16 +0200

    update interface with crud funktionality

--- backend/src/core/repositories/group.repository.interface.ts
+++ backend/src/core/repositories/group.repository.interface.ts
@@ -1,4 +1,8 @@
+import { Group } from '../domain/group';
+
 export interface IGroupRepository {
   create(group: Group): Promise<Group>;
   findById(id: string): Promise<Group | null>;
+  update(id: string, group: Group): Promise<Group | null>;
+  delete(id: string): Promise<boolean>;
 }
