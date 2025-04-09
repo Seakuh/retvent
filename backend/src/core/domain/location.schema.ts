345ec4cfc5bd627ab56f4f0288eab2a08426ca05
Seakuh <eteranimal@protonmail.com>
2025-04-09 10:19:50 +0200

    linting

--- backend/src/core/domain/location.schema.ts
+++ backend/src/core/domain/location.schema.ts
@@ -1,22 +1,25 @@
 import { Schema } from 'mongoose';
 
-export const LocationSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  logoUrl: { type: String, required: true },
-  address: { type: String, required: true },
-  coordinates: {
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true }
+export const LocationSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    logoUrl: { type: String, required: true },
+    address: { type: String, required: true },
+    coordinates: {
+      latitude: { type: Number, required: true },
+      longitude: { type: Number, required: true },
+    },
+    website: String,
+    socialMediaLinks: {
+      instagram: String,
+      facebook: String,
+      twitter: String,
+    },
+    ownerId: { type: String, required: true },
+    followerIds: [String],
+    likeIds: [String],
+    eventsCount: { type: Number, default: 0 },
   },
-  website: String,
-  socialMediaLinks: {
-    instagram: String,
-    facebook: String,
-    twitter: String
-  },
-  ownerId: { type: String, required: true },
-  followerIds: [String],
-  likeIds: [String],
-  eventsCount: { type: Number, default: 0 }
-}, { timestamps: true }); 
+  { timestamps: true },
+);
