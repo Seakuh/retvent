ca3ca40c6b7cf2ef55e10e81df136f54985ba4ea
Seakuh <eteranimal@protonmail.com>
2025-08-02 12:55:41 +0200

    add cr code

--- frontend/src/pages/TicketPage.tsx
+++ frontend/src/pages/TicketPage.tsx
@@ -1,6 +1,7 @@
+import { QRCodeCanvas } from "qrcode.react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Event, Ticket } from "../utils";
+import { Event, FRONTEND_URL, Ticket } from "../utils";
 import { getEvent, getTicket } from "./service";
 import "./TicketPage.css";
 
@@ -22,18 +23,34 @@ export const TicketPage = () => {
     });
   }, [ticketId]);
   return (
-    <div>
+    <div className="ticket-page">
       <div>
-        <img src={event?.imageUrl} alt="eventimage" />
-        <p>{ticket?.email}</p>
-        <p>{ticket?.status}</p>
-        <p>{event?.title}</p>
-        <p>{event?.description}</p>
-        <p>{event?.startDate?.toLocaleString()}</p>
-        <p>{event?.endDate?.toLocaleString()}</p>
-        <p>{event?.locationId}</p>
-        <p>{event?.price}</p>
-        <p>{event?.ticketLink}</p>
+        <div className="qr-code-ticket-container">
+          <QRCodeCanvas
+            value={`${FRONTEND_URL}ticket/${ticket?.ticketId}`}
+            size={420}
+            level="H"
+            bgColor="#FFFFFF"
+            fgColor="#000000"
+            includeMargin={true}
+            style={{
+              borderRadius: "10px",
+              padding: "0.5rem",
+              backgroundColor: "black",
+              boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+            }}
+          />
+        </div>{" "}
+        <img className="ticket-image" src={event?.imageUrl} alt="eventimage" />
+        <p className="ticket-email">{ticket?.email}</p>
+        <p className="ticket-status">{ticket?.status}</p>
+        <p className="ticket-title">{event?.title}</p>
+        <p className="ticket-description">{event?.description}</p>
+        <p className="ticket-startDate">{event?.startDate?.toLocaleString()}</p>
+        <p className="ticket-endDate">{event?.endDate?.toLocaleString()}</p>
+        <p className="ticket-locationId">{event?.locationId}</p>
+        <p className="ticket-price">{event?.price}</p>
+        <p className="ticket-ticketLink">{event?.ticketLink}</p>
       </div>
     </div>
   );
