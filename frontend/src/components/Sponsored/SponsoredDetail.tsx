7f68b7ac1ea61f6dda8ef3be3103537a79595e38
Seakuh <eteranimal@protonmail.com>
2025-10-10 17:29:40 +0200

    show hetzner videos

--- frontend/src/components/Sponsored/SponsoredDetail.tsx
+++ frontend/src/components/Sponsored/SponsoredDetail.tsx
@@ -5,33 +5,36 @@ export const SponsoredDetail = ({ size = "m" }: { size?: "s" | "m" | "l" }) => {
     {
       id: 1,
       title: "Schallwerk Headphones",
-      video:
-        "https://event-scanner.b-cdn.net/event-scanner-advertisement/videos/tmprqg0ybrh.mp4",
+      video: "https://imagebucket.hel1.your-objectstorage.com/tmprqg0ybrh.mp4",
       link: "https://schallwerk.eu?ref=OTQxMTg5LjM0OTcuMTE3ODg0My5U",
     },
     {
       id: 2,
       title: "Lighter DIY",
-      video:
-        "https://event-scanner.b-cdn.net/event-scanner-advertisement/videos/tmpdlv1tkdy.mp4",
+      video: "https://imagebucket.hel1.your-objectstorage.com/tmpdlv1tkdy.mp4",
       link: "https://www.instagram.com/sturmzx2/",
     },
   ];
 
-  const randomAd = ads[Math.floor(Math.random() * ads.length)];
+  const ad = ads[Math.floor(Math.random() * ads.length)];
 
   return (
     <div
       className={`sponsored-detail ${size}-size`}
-      onClick={() => window.open(randomAd.link, "_blank")}
+      onClick={() => window.open(ad.link, "_blank")}
     >
-      <iframe
-        src={randomAd.video}
-        className="sponsored-video"
-        allow="autoplay; fullscreen; picture-in-picture"
-        allowFullScreen
-        title={randomAd.title}
-      />
+      <div className="sponsored-watermark">sponsored</div>
+
+      <div className="video-frame">
+        <video
+          src={ad.video}
+          className="sponsored-video"
+          autoPlay
+          muted
+          loop
+          playsInline
+        />
+      </div>
     </div>
   );
 };
