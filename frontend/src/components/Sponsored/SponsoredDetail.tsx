efb8768e864fad23f462aa40d500a7512696c870
Seakuh <eteranimal@protonmail.com>
2025-10-10 16:05:57 +0200

    adjust add

--- frontend/src/components/Sponsored/SponsoredDetail.tsx
+++ frontend/src/components/Sponsored/SponsoredDetail.tsx
@@ -1,19 +1,35 @@
 import "./SponsoredDetail.css";
 
 export const SponsoredDetail = () => {
+  const ads = [
+    {
+      id: 1,
+      title: "Schallwerk Headphones",
+      video:
+        "https://iframe.mediadelivery.net/play/509542/e30a068e-975d-4fe8-9b3b-4ae267792312",
+      link: "https://schallwerk.eu?ref:OTQxMTg5LjM0OTcuMTE3ODg0My5U",
+    },
+    {
+      id: 2,
+      title: "Lighter DIY",
+      video:
+        "https://iframe.mediadelivery.net/play/509542/b15cf162-4b5f-4aa3-8177-09ea80dbcf01",
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    },
+  ];
+
+  const randomAd = ads[Math.floor(Math.random() * ads.length)];
+
   return (
     <div
       className="sponsored-detail"
       onClick={() => {
-        window.open(
-          "https://schallwerk.eu?ref:OTQxMTg5LjM0OTcuMTE3ODg0My5U",
-          "_blank"
-        );
+        window.open(randomAd.link, "_blank");
       }}
     >
-      <div className="sponsored-watermark">Sponsored</div>
+      <div className="sponsored-watermark">sponsored</div>
       <video
-        src="https://iframe.mediadelivery.net/play/509542/e30a068e-975d-4fe8-9b3b-4ae267792312"
+        src={randomAd.video}
         className="sponsored-video"
         autoPlay
         muted
